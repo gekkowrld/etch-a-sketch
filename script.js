4e91fb19a0ff72b5139b9d50b3d0f13ddd2d6f52
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-01-22 20:59:10 +0300

    Basic drawing functions working

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const squarePad = document.querySelector('.square-pad');
+const resetBtn = document.querySelector('.reset');
+const size = document.querySelector('#size');
+const sizeValue = document.querySelector('.size-value');
+const theColor = document.querySelector('#color');
+const randomColor = document.querySelector('.random-color');
+
 
 for(let i = 0; i < 256; i++){
     const smallGrids = document.createElement('div');
@@ -8,10 +14,34 @@ for(let i = 0; i < 256; i++){
 
 const smallGrids = document.querySelectorAll('.small-grids');
 
-let isSketching = false;
+smallGrids.forEach(smallGrid => {
+    smallGrid.style.cssText = `width: ${size.value}px; height: ${size.value}px`;
+    sizeValue.innerText = `${size.value}px`;
 
-squarePad.addEventListener('mousedown', () => {
-    smallGrids.forEach(smallGrid => smallGrid.addEventListener('mouseover', () => {
-        smallGrid.style.cssText = 'background-color: green';
-    })) 
 })
+
+size.addEventListener('click', () => {
+    smallGrids.forEach(smallGrid => {
+        smallGrid.style.cssText = `width: ${size.value}px; height: ${size.value}px`;
+        sizeValue.innerText = `${size.value}px`;
+    })
+})
+
+resetBtn.addEventListener('click', () => {
+    location.reload();
+})
+
+const sketch = function(e){
+    e.target.style.backgroundColor = theColor.value;
+}
+const nullSketch = function(e){
+    e.target.style.backgroundColor = '';
+}
+squarePad.addEventListener('mousedown', () => {
+  smallGrids.forEach(smallGrid => smallGrid.addEventListener('mousemove', sketch));
+})
+squarePad.addEventListener('mouseup', () => {
+    smallGrids.forEach(smallGrid => smallGrid.addEventListener('mouseup', nullSketch));
+  })
+  
+
